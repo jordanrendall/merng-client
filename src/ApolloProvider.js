2a230eb41634ccd81f638ed519ac1071455bdffc
Jordan Rendall <thomasjrendall@gmail.com>
2021-01-17 12:42:44 -0500

    removed .env

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -10,6 +10,9 @@ import {
 } from '@apollo/client';
 require('dotenv').config();
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.BACKEND_URL_DEV);
+console.log(process.env.BACKEND_URL_PROD);
 const httpLink = new HttpLink({
   uri:
     process.env.NODE_ENV === 'production'
